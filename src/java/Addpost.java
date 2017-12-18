0c1cb8c999fcc71f9a5c32f322b79e3cc8cf6bee
kawrshy <kawrshy@DESKTOP-33U4B24>
2017-12-18 06:27:25 -0800

    modified kza 5ra

--- src/java/Addpost.java
+++ src/java/Addpost.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
@@ -21,31 +12,17 @@ import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
 
 
-/**
- *
- * @author Antr
- */
 @WebServlet(urlPatterns = {"/Addpost"})
-@MultipartConfig(maxFileSize = 16177216)
+@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 2, //2 MB
+        maxFileSize = 1024 * 1024 * 10, //10 MB
+        maxRequestSize = 1024 * 1024 * 50)
 public class Addpost extends HttpServlet {
-
-    private static final String SAVE_DIR = "images";
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException, SQLException, ClassNotFoundException {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            String houseSize = request.getParameter("house_size");
+        PrintWriter out = response.getWriter();
+        Part part = request.getPart("houseImg");
+        String houseSize = request.getParameter("house_size");
             String housePrice = request.getParameter("house_prise");
             String houseType = request.getParameter("house_type");
             String houseLocattion = request.getParameter("house_locattion");
@@ -77,82 +54,29 @@ public class Addpost extends HttpServlet {
             advertisment.setAccountId_fk(userid);
             out.println("hhhhhh => " + advertisment.getAccountId_fk());
             advertisment.AddAdvertisment();
-            Part part = request.getPart("houseImg");
-            InputStream is = part.getInputStream();
-            String savePath = "C:" + File.separator + SAVE_DIR;
-            File fileSaveDir = new File(savePath);
-            if (!fileSaveDir.exists()) {
+        String fileName = extractFileName(part);
+        String savePath = "E:\\kolya\\Sna 4\\IA\\project\\House-Shop\\web\\scr\\" + File.separator;
+        File fileSaveDir = new File(savePath);
+        if(!fileSaveDir.exists()){
+            fileSaveDir.mkdir();
+        }
+        try{
+            part.write(savePath + File.separator+fileName);
+        } catch (Exception ex){
+            out.println(ex.getMessage());
+        }
+    }
 
-                fileSaveDir.mkdir();
-
+    private String extractFileName(Part part) {
+        String contentDisplay = part.getHeader("content-disposition");
+        String[] items = contentDisplay.split(";");
+        for (String str : items) {
+            if (str.trim().startsWith("filename")) {
+                return str.substring(str.indexOf("=") + 2, str.length() - 1);
             }
-            String photoName= extractFileName(part);
-            out.print(photoName);
-            part.write(savePath + File.separator + photoName);
-              
-//              response.sendRedirect("JSP/Home.jsp");
-            //Profile
         }
+        return "NoFile";
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        try {
-            processRequest(request, response);
-        } catch (SQLException ex) {
-            Logger.getLogger(Addpost.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(Addpost.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        try {
-            processRequest(request, response);
-        } catch (SQLException ex) {
-            Logger.getLogger(Addpost.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(Addpost.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-private String extractFileName(Part part) {
-    String contentDisp = part.getHeader("content-disposition");
-    String[] items = contentDisp.split(";");
-    for (String s : items) {
-        if (s.trim().startsWith("filename")) {
-            return s.substring(s.indexOf("=") + 2, s.length()-1);
-        }
-    }
-    return "";
 }
-}
+
+
